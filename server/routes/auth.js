0fcca9464e16123d0d7267f0641473b661363641
Mhossain028 <meraj.hossain028@gmail.com>
2022-12-13 14:38:25 +0600

    Creating user model

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../models/user');
 
 const authRouter = express.Router();
 
@@ -8,6 +9,8 @@ const authRouter = express.Router();
 authRouter.post('/api/signup', (req, res) => {
     //get the data from the CLIENT
     const {name, email, password} = req.body;
+
+    User.findOne()
     //post that data in database
     //return that data to the user
     
